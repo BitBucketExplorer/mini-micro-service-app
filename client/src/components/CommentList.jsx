fe5b485754c367e4010bb1e6bf354b9a8d8fa336
vivek yadav <vivek.yadav@spraxa.com>
2023-05-13 14:47:34 +0530

    Removed code for extra api-hit

--- client/src/components/CommentList.jsx
+++ client/src/components/CommentList.jsx
@@ -1,26 +1,4 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import { comment_url } from "../config";
-
 const CommentList = ({ comments }) => {
-	// const [commentList, setCommentList] = useState([]);
-	// const config = {
-	// 	headers: {
-	// 		"Content-Type": "application/json",
-	// 	},
-	// };
-
-	// const fetchPosts = async () => {
-	// 	const response = await axios.get(
-	// 		`${comment_url}/posts/${postId}/comments`,
-	// 		config
-	// 	);
-	// 	setCommentList(response.data);
-	// };
-	// useEffect(() => {
-	// 	fetchPosts();
-	// }, []);
-
 	const renderedComments = comments.map((comment) => {
 		return (
 			<li className='list-group-item' key={comment.id}>
